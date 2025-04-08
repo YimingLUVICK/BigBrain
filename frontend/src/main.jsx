f95171f5dd07ccf837ebda3b2c5443be6e1a6ef7
YimingLUVICK <lyiming700@gmail.com>
2025-04-08 23:57:41 +1000

    import tailwindcss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const link = document.createElement('link');
+link.href = 'https://cdn.tailwindcss.com';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
